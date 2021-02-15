d6274341324852947d51158e86f3e091a32884a3
Amilkar Munoz <yellowballpython@gmail.com>
2021-02-15 03:36:28 -0600

    Account Forms Added
    Se agregan los forms para editar la cuenta y elegir el rol. Además de sus respectivos templates.

--- account/forms.py
+++ account/forms.py
@@ -1,11 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import MyUser, Rol
+from .models import MyUser, Account, Rol
 
 class MyUserCreationForm(UserCreationForm):
     class Meta:
         model = MyUser
         fields = ('username', 'password1', 'password2', 'first_name')
+
+class EditAccountForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ('image', 'description')
 
 class RolForm(forms.ModelForm):
     class Meta:
