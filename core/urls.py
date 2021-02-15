968c04dd3e5ce60e20db812680c5c65b0d25f238
Amilkar Munoz <yellowballpython@gmail.com>
2021-02-15 03:45:39 -0600

    static to URLpatterns
    Se agrega el método static from django.conf.urls.static al urlspatterns del core.

--- core/urls.py
+++ core/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('', include('helpdesk.urls')),
     path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
