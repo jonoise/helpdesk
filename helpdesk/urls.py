02a54b916c539395c5e9177aa83cb8ddc197682c
Amilkar Munoz <yellowballpython@gmail.com>
2021-02-15 03:47:30 -0600

    New Ticket
    Se agrega el url, view y template.

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -4,8 +4,9 @@ from . import views
 app_name = 'helpdesk'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('helpdesk/', views.helpdesk, name='helpdesk'),
+    path('dashboard/', views.dashboard, name='helpdesk'),
+    path('new-ticket/', views.new_ticket, name='new_ticket'),
     path('<int:year>/<int:month>/<int:day>/<str:code>',
-         views.helpdesk, name='ticket_detail'),
+         views.ticket_detail, name='ticket_detail'),
     
 ]
--- helpdesk/views.py
+++ helpdesk/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Ticket, Comment, Attachment, Log, VacationRequest, Vacation
+from .forms import TicketForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 
@@ -8,7 +9,7 @@ def index(request):
     return render(request, 'helpdesk/index.html')
 
 @login_required
-def helpdesk(request):
+def dashboard(request):
     tickets = Ticket.objects.filter(owner=request.user)
     comments = Comment.objects.filter(owner=request.user)
 
@@ -17,10 +18,20 @@ def helpdesk(request):
         'comments': comments,
     }
 
-    return render(request, 'helpdesk/helpdesk.html', context)
+    return render(request, 'helpdesk/dashboard.html', context)
 
 @login_required
-def detail(request, year, month, day, code):
+def new_ticket(request):
+    form = TicketForm()
+    if request.method == 'POST':
+        form = TicketForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('helpdesk:dashboard')
+
+
+@login_required
+def ticket_detail(request, year, month, day, code):
     ticket = get_object_or_404(Ticket(created__year=year,
                                       created__month=month,
                                       created__day=day,
