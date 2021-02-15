0e1e7b758a10cb2d8fb932e53bd4779cab17a3ac
Amilkar Munoz <yellowballpython@gmail.com>
2021-02-15 03:40:14 -0600

    Account Urls & Views

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 app_name = 'account'
 urlpatterns = [
     path('create/', views.create_user, name='create'),
-    path('login/', views.create_user, name='login'),
+    path('edit/', views.edit, name='edit'),
+    path('login/', LoginView.as_view(template_name='account/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='account/logout.html'), name='logout'),
     path('detail/', views.create_user, name='detail'),
     path('rol/', views.user_rol, name='rol'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import MyUserCreationForm
+from .forms import MyUserCreationForm, RolForm, EditAccountForm
 from .models import MyUser, Account
 
 # Create your views here.
@@ -18,7 +18,7 @@ def create_user(request):
             )
             new_user.save()
             message = messages.success(request, 'Usuario, creado')
-            return redirect('helpdesk:index', messages=message)
+            return redirect('account:rol', messages=message)
 
     context = {
         'form':form
@@ -30,14 +30,30 @@ def login(request):
     pass
 
 @login_required
-def detail(request):
+def edit(request):
     user = get_object_or_404(MyUser, username=request.user.username)
+    form = EditAccountForm(instance=user.account)
+    
+    if request.method == 'POST':
+        form = EditAccountForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('helpdesk:helpdesk')
 
     context = {
-        'user':user
+        'form':form
     }
 
-    return render(request, 'account/detail.html', context)
+    return render(request, 'account/edit.html', context)
 
+
+
+
+@login_required()
 def user_rol(request):
-    pass
+    form = RolForm(instance=request.user.account.rol)
+    if request.method == 'POST':
+        form = RolForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('helpdesk:helpdesk')
