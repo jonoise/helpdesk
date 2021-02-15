3264722280f7b1008163c83ad21a6f2c75d0f96f
Amilkar Munoz <yellowballpython@gmail.com>
2021-02-15 03:44:27 -0600

    MyUser Settings
    Se agregan las dos variables principales para manejar un AbstractUser: LOGIN_URL, LOGIN_REDIRECT_URL.

--- core/settings.py
+++ core/settings.py
@@ -125,6 +125,10 @@ USE_L10N = True
 USE_TZ = False
 
 
+# Authentication
+LOGIN_URL = 'account:login'
+LOGIN_REDIRECT_URL = 'helpdesk:helpdesk'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
